76d801bced164308de00ac82897e6b4aa96a31d8
yamadamasashi <shisama94@gmail.com>
2021-11-30 09:34:44 +0900

    getStaticProps

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -2,36 +2,34 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = path.join(process.cwd(), 'posts')
+const postsDirectory = path.join( process.cwd(), 'posts' )
 
 export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
-    // Remove ".md" from file name to get id
+  // /posts　配下のファイル名を取得する
+    const fileNames = fs.readdirSync( postsDirectory )
+    const allPostsData = fileNames.map( fileName => {
+    // id を取得するためにファイル名から ".md" を削除する
     const id = fileName.replace(/\.md$/, '')
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-    // Use gray-matter to parse the post metadata section
+    // マークダウンファイルを文字列として読み取る
+    const fullPath = path.join( postsDirectory, fileName )
+        const fileContents = fs.readFileSync( fullPath, 'utf8' )//ファイルの中身を取得
+        
+    // 投稿のメタデータ部分を解析するために gray-matter を使う
     const matterResult = matter(fileContents)
 
-    // Combine the data with the id
+    // データを id と合わせる
     return {
       id,
       ...matterResult.data
     }
   })
-  // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
-    if (a < b) {
+  // 投稿を日付でソートする
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
       return 1
-    } else if (a > b) {
-      return -1
     } else {
-      return 0
+      return -1
     }
   })
 }
